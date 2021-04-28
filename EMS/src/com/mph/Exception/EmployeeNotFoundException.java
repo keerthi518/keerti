179648a3547ccebc10d222df556b19417f9074bf
keerthi518 <yerramchettikeerthi@gmail.com>
2021-04-28 15:45:27 +0530

    done

---
+++ EMS/src/com/mph/Exception/EmployeeNotFoundException.java
@@ -0,0 +1,14 @@
+package com.mph.Exception;
+public class EmployeeNotFoundException {
+    public EmployeeNotFoundException() {
+        
+    }
+    @Override
+    public String toString() {
+        return "UserNotFoundException []";
+    }
+    public void EmployeeNotFoundException() {
+        System.out.println(" User Not Found.. Enter correct credentials ");
+        
+    }
+}
